7682af2e885f1523c3f61b1a1856628bf6e4ce28
skycoco <skycoco1994@gmail.com>
2024-02-18 21:33:28 +1300

    init validator

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cs_fhir
+
+## Validator
+- download a validator from [HL7 validator](https://hl7.org/fhir/validator/)
+- check the [validation code](https://hl7.org/fhir/R4/validation.html)
+- [us.core](https://www.hl7.org/fhir/us/core/StructureDefinition-us-core-patient.html)
+- [online validator](https://inferno.healthit.gov/validator/)
+
+```sh
+java -jar .\validator_cli.jar .\patient.json -version 4.0.1
+```
---
+++ cs_fhir_profile/cs_fhir_profile/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+
+namespace cs_fhir_profile
+{
+    /// <summary>
+    ///  Program to test working with IGs/Profile
+    /// </summary>
+    /// <param name="patientJsonFilename">Output JSON file of our patient</param>
+    /// 
+    public class Program
+    {
+        public static void Main(
+            string patientJsonFilename = ""    
+        )
+        {
+
+            if (string.IsNullOrEmpty(patientJsonFilename))
+            {
+                patientJsonFilename = "C:/Users/lgao142/OneDrive - The University of Auckland/Desktop/c#/cs_fhir/cs_fhir_profile/cs_fhir_profile/patient.json";
+            }
+
+            Patient patient = new Patient() 
+            { 
+                Meta = new Meta()
+                {
+                    Profile = new List<string>()
+                    {
+                        "http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient",
+                    }
+                }
+            };
+
+            FhirJsonSerializer fhirJsonSerializer = new FhirJsonSerializer(new SerializerSettings()
+            {
+                Pretty = true,
+            });
+
+            string json = fhirJsonSerializer.SerializeToString(patient);
+
+            File.WriteAllText(patientJsonFilename, json);
+        }
+    }
+}
